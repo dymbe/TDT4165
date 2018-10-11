f74a6ef11356257da2076f96ce4c9b0d13222d1c
Sindre Stephansen <sindre@sindrestephansen.com>
2018-10-11 11:47:27 +0200

    Implement transaction in task 1

--- scala_project/task1-project/src/main/scala/Transaction.scala
+++ scala_project/task1-project/src/main/scala/Transaction.scala
@@ -6,21 +6,22 @@ object TransactionStatus extends Enumeration {
 }
 
 class TransactionQueue {
+    private val queue: mutable.Queue[Transaction] = mutable.Queue()
 
     // Remove and return the first element from the queue
-    def pop: Transaction = ???
+    def pop: Transaction = queue.dequeue()
 
     // Return whether the queue is empty
-    def isEmpty: Boolean = ???
+    def isEmpty: Boolean = queue.isEmpty
 
     // Add new element to the back of the queue
-    def push(t: Transaction): Unit = ???
+    def push(t: Transaction): Unit = queue.enqueue(t)
 
     // Return the first element from the queue without removing it
-    def peek: Transaction = ???
+    def peek: Transaction = queue.front
 
     // Return an iterator to allow you to iterate over the queue
-    def iterator: Iterator[Transaction] = ???
+    def iterator: Iterator[Transaction] = queue.iterator
 }
 
 class Transaction(val transactionsQueue: TransactionQueue,
@@ -33,23 +34,30 @@ class Transaction(val transactionsQueue: TransactionQueue,
   var status: TransactionStatus.Value = TransactionStatus.PENDING
 
   override def run: Unit = {
-
       def doTransaction() = {
           from withdraw amount
           to deposit amount
       }
 
-      if (from.uid < to.uid) from synchronized {
-          to synchronized {
-            doTransaction
-          }
-      } else to synchronized {
-          from synchronized {
-            doTransaction
+      for (_ <- 0 until allowedAttemps) {
+          try {
+              if (from.uid < to.uid) from synchronized {
+                  to synchronized {
+                      doTransaction
+                  }
+              } else to synchronized {
+                  from synchronized {
+                      doTransaction
+                  }
+              }
+
+              status = TransactionStatus.SUCCESS
+              return
+          } catch {
+              case _: Throwable => _
           }
       }
 
-      // Extend this method to satisfy requirements.
-
-    }
+      status = TransactionStatus.FAILED
+  }
 }
