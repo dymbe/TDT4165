7f4e8f85dfbd958e6b4d83f9111b56a7ecd0424f
Solve\Soelve <srb.hunvik@gmail.com>
2018-10-11 11:20:04 +0200

    Implement generateuid function

--- scala_project/task1-project/src/main/scala/Bank.scala
+++ scala_project/task1-project/src/main/scala/Bank.scala
@@ -1,8 +1,9 @@
 import scala.concurrent.forkjoin.ForkJoinPool
+import java.util.concurrent.atomic.AtomicInteger
 
 class Bank(val allowedAttempts: Integer = 3) {
 
-    private val uid = ???
+    private val uid = new AtomicInteger()
     private val transactionsQueue: TransactionQueue = new TransactionQueue()
     private val processedTransactions: TransactionQueue = new TransactionQueue()
     private val executorContext = ???
@@ -12,8 +13,15 @@ class Bank(val allowedAttempts: Integer = 3) {
         transactionsQueue, processedTransactions, from, to, amount, allowedAttempts)
     }
 
-    // Hint: use a counter 
-    def generateAccountId: Int = ???
+    // Hint: use a counter
+    def generateAccountId: Int = uid.getAndIncrement()
+
+
+        /*uidCount.synchronized {
+        val newUid = uidCount + 1
+        uidCount = newUid
+        newUid
+    }*/
 
     private def processTransactions: Unit = ???
 
